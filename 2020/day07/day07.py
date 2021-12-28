0257d6a19aa2da03c64790f26def2f249bca4480
mukundv <mukund.venkatachar@gmail.com>
2021-12-28 15:01:31 +1100

    Day 07
    Signed-off-by: mukundv <mukund.venkatachar@gmail.com>

--- 2020/day07/day07.py
+++ 2020/day07/day07.py
@@ -1,4 +1,7 @@
+import collections
 import os
+import re
+from typing import Any, Match
 
 from aocd import get_data
 from dotenv import load_dotenv
@@ -27,12 +30,66 @@ def run(data: str = None, part: int = None):
             return part2(data.splitlines())
 
 
-def part1(data):
-    return data
+def get_pattern():
+    return re.compile('^([^ ]+ [^ ]+) bags contain (.*)$')
 
 
-def part2(data):
-    return data
+pattern = get_pattern()
+
+
+def get_bag():
+    return re.compile(r'(\d+) ([^ ]+ [^ ]+)')
+
+
+bag = get_bag()
+
+
+def part1(data: list) -> int:
+    parents = collections.defaultdict(list)
+    for line in data:
+        get_parents(line, parents)
+    total_colours = set()
+    todo = parents['shiny gold']
+    while todo:
+        colour = todo.pop()
+        if colour not in total_colours:
+            total_colours.add(colour)
+            todo.extend(parents[colour])
+    return len(total_colours)
+
+
+def get_parents(line: str, parents: collections.defaultdict[Any, list]):
+    k, match = get_match(line)
+    targets = get_targets(match)
+    for _, colour in targets:
+        parents[colour].append(k)
+
+
+def get_match(line: str) -> tuple[str | Any, Match[str] | None]:
+    match = pattern.match(line)
+    assert match
+    k = match[1]
+    return k, match
+
+
+def get_targets(match: Match[str]) -> list:
+    return [(int(n), tp) for n, tp in bag.findall(match[2])]
+
+
+def part2(data: list) -> int:
+    colours = {}
+    for line in data:
+        k, match = get_match(line)
+        colours[k] = get_targets(match)
+    total_bags = 0
+    todo = [(1, 'shiny gold')]
+    while todo:
+        n, colour = todo.pop()
+        total_bags += n
+        for n_i, colour_i in colours[colour]:
+            todo.append((n * n_i, colour_i))
+    total_bags -= 1
+    return total_bags
 
 
 if __name__ == '__main__':
