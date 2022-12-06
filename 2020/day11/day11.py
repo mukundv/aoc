f136a582b49db26101de54790920e8f20085824e
mukundv <mukund.venkatachar@gmail.com>
2022-12-06 17:05:29 +1100

    dependabot
    Signed-off-by: mukundv <mukund.venkatachar@gmail.com>

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -1,9 +1,9 @@
 import os
+from functools import cache
+from typing import Optional, Tuple
 
 from aocd import get_data
 from dotenv import load_dotenv
-
-from utils import generate_readme
 
 day = int(os.getcwd()[-2:])
 year = int(os.getcwd()[-10:][:4])
@@ -17,18 +17,67 @@ def get_session() -> str:
 def run(data: str = None, part: int = None):
     if not data:
         if part == 1:
-            return part1(get_data(get_session(), day=day, year=year).splitlines())
+            return part1(get_data(get_session(), day=day, year=year))
         else:
-            return part2(get_data(get_session(), day=day, year=year).splitlines())
+            return part2(get_data(get_session(), day=day, year=year))
     else:
         if part == 1:
-            return part1(data.splitlines())
+            return part1(data)
         else:
-            return part2(data.splitlines())
+            return part2(data)
+
+
+def _index(lines, y, x):
+    if y < 0:
+        return ' '
+    elif y >= len(lines):
+        return ' '
+    elif x < 0:
+        return ' '
+    elif x >= len(lines[0]):
+        return ' '
+    return lines[y][x]
+
+
+@cache
+def _adjacent(lines, y, x):
+    @cache
+    def _inner():
+        for y_i in range(y - 1, y + 2):
+            for x_i in range(x - 1, x + 2):
+                if (y_i, x_i) != (y, x):
+                    yield _index(lines, y_i, x_i)
+
+    return tuple(_inner())
 
 
 def part1(data):
-    return data
+    lines = tuple(data.splitlines())
+    prev: Optional[Tuple[str, ...]] = None
+    while lines != prev:
+        prev = lines
+        new_lines = []
+        for y, line in enumerate(lines):
+            line_c = []
+            for x, c in enumerate(line):
+                if c == 'L':
+                    if _adjacent(lines, y, x).count('#') == 0:
+                        line_c.append('#')
+                    else:
+                        line_c.append('L')
+                elif c == '#':
+                    if _adjacent(lines, y, x).count('#') >= 4:
+                        line_c.append('L')
+                    else:
+                        line_c.append('#')
+                else:
+                    line_c.append(c)
+
+                new_lines.append(''.join(line_c))
+
+                lines = tuple(new_lines)
+
+    return sum(line.count('#') for line in lines)
 
 
 def part2(data):
@@ -37,5 +86,5 @@ def part2(data):
 
 if __name__ == '__main__':
     print(f'Part 1: {run(data=None, part=1)}')
-    print(f'Part 2: {run(data=None, part=2)}')
-    generate_readme("README", year, day, '../')
+    # print(f'Part 2: {run(data=None, part=2)}')
+    # generate_readme("README", year, day, '../')
